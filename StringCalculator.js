7e08f296f0775a653ef06310ef142925116feeb9
guptamuskan988 <69482363+guptamuskan988@users.noreply.github.com>
2024-10-15 21:25:08 +0530

    Handling new lines as delimiters

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,21 +1,24 @@
-//Handling the string seperated by multiple commas
+//Handling new lines as delimiters
 class StringCalculator {
     add(numbers) {
         if (numbers === "") {
             return 0;
         }
         
-        const numberArray = numbers.split(",");
+        numbers = numbers.replace(/\n/g, ",");
+        const numberArray = number.split(",");
         let sum = 0;
         
         for (let number of numberArray) {
-            sum += parseInt(number, 10);//Short Hand Operators
+            sum += parseInt(number, 10);
         }
         
         return sum;
     }
 }
 const calculator = new StringCalculator();
-console.log(calculator.add(""));     
-console.log(calculator.add("1"));     
-console.log(calculator.add("1,2,3"));
+console.log(calculator.add(""));          
+console.log(calculator.add("1"));       
+console.log(calculator.add("1,2,3"));  
+console.log(calculator.add("-1,-3")); //Will fail because we are not handling negative numbers
+console.log(calculator.add("1\n2,3")); 
