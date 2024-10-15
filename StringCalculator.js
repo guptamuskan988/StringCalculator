cd39f55177b7c419aa161992a616ca0850f239d8
guptamuskan988 <69482363+guptamuskan988@users.noreply.github.com>
2024-10-15 21:38:25 +0530

    Changed the delimiters to ; and | and for TDD I have changed delimiter but not changed the delimiters in inputs provided

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,12 +1,19 @@
-//Updated number to numbers in line no 9 and added test case for negative number which will add the negative numbers without throwing exception
+// Changed the delimiters to ; and | and for TDD I have changed delimiter but not changed the delimiters in inputs provided
 class StringCalculator {
     add(numbers) {
         if (numbers === "") {
             return 0;
         }
-        
-        numbers = numbers.replace(/\n/g, ",");
-        const numberArray = numbers.split(",");
+
+        let delimiter = ",";
+        if (numbers.startsWith("//")) {
+            const delimiterEnd = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterEnd);
+            numbers = numbers.substring(delimiterEnd + 1);
+        }
+
+        numbers = numbers.replace(/\n/g, delimiter);
+        const numberArray = numbers.split(delimiter);
         let sum = 0;
         
         for (let number of numberArray) {
@@ -17,8 +24,10 @@ class StringCalculator {
     }
 }
 const calculator = new StringCalculator();
-console.log(calculator.add(""));          
-console.log(calculator.add("1"));       
-console.log(calculator.add("1,2,3"));  
-console.log(calculator.add("-1,-3")); //Will add the negative numbers here because we are not handling negative numbers test case
-console.log(calculator.add("1\n2,3")); 
+console.log(calculator.add(""));           
+console.log(calculator.add("1"));            
+console.log(calculator.add("1,2,3"));        
+console.log(calculator.add("1\n2,3"));  
+console.log(calculator.add("//|\n1,2,3"));//TDD test case which will provide output as 1 because delimiter is changed to | this but seperated by ,
+console.log(calculator.add("//;\n1;2"));  //Changed the delimiter to ;   
+console.log(calculator.add("//|\n1|2|3"));//Changed the delimiter to |
