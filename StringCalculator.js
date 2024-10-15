7e2bf53d4fb1edd25e59f3af95fdbc271cb88717
guptamuskan988 <69482363+guptamuskan988@users.noreply.github.com>
2024-10-15 20:54:08 +0530

    Update StringCalculator.js
    Creating the basic StringCalculator method to accept string and return zero if string is empty  and also handling single number input

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,1 +1,13 @@
+//Creating the basic StringCalculator method to accept string and return zero is string is empty 
+class StringCalculator {
+    add(numbers) {
+        if (numbers === "") {
+            return 0;
+        }
+        return parseInt(numbers, 10);
+    }
+}
 
+const calculator = new StringCalculator();
+console.log(calculator.add(""));  
+console.log(calculator.add("1"));
