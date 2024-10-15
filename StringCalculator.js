0d8c76a8e1e51fbd0759e64435b115ea3c3e15f1
guptamuskan988 <69482363+guptamuskan988@users.noreply.github.com>
2024-10-15 21:14:34 +0530

    Handling the string seperated by multiple commas
    Handling the string seperated by multiple commas

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,13 +1,21 @@
-//Creating the basic StringCalculator method to accept string and return zero is string is empty 
+//Handling the string seperated by multiple commas
 class StringCalculator {
     add(numbers) {
         if (numbers === "") {
             return 0;
         }
-        return parseInt(numbers, 10);
+        
+        const numberArray = numbers.split(",");
+        let sum = 0;
+        
+        for (let number of numberArray) {
+            sum += parseInt(number, 10);//Short Hand Operators
+        }
+        
+        return sum;
     }
 }
-
 const calculator = new StringCalculator();
-console.log(calculator.add(""));  
-console.log(calculator.add("1"));
+console.log(calculator.add(""));     
+console.log(calculator.add("1"));     
+console.log(calculator.add("1,2,3"));
