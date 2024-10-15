ba033c4686b672a89ba6159fae0e5dbc5ca1ef0e
guptamuskan988 <69482363+guptamuskan988@users.noreply.github.com>
2024-10-15 21:45:28 +0530

    Added the negative numbers exceptions with try and catch block

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,4 +1,4 @@
-// Changed the delimiters to ; and | and for TDD I have changed delimiter but not changed the delimiters in inputs provided
+// Added the negative numbers exceptions with try and catch block
 class StringCalculator {
     add(numbers) {
         if (numbers === "") {
@@ -15,15 +15,24 @@ class StringCalculator {
         numbers = numbers.replace(/\n/g, delimiter);
         const numberArray = numbers.split(delimiter);
         let sum = 0;
+       const negativeNumbers = [];
         
-        for (let number of numberArray) {
-            sum += parseInt(number, 10);
+       for (let number of numberArray) {
+            const parsedNumber = parseInt(number, 10);
+            if (parsedNumber < 0) {
+                negativeNumbers.push(parsedNumber);
+            }
+            sum += parsedNumber;
         }
-        
+         if (negativeNumbers.length > 0) {
+            throw new Error(`negative numbers not allowed: ${negativeNumbers.join(", ")}`);
+        }
+
         return sum;
     }
 }
 const calculator = new StringCalculator();
+try {
 console.log(calculator.add(""));           
 console.log(calculator.add("1"));            
 console.log(calculator.add("1,2,3"));        
@@ -31,3 +40,7 @@ console.log(calculator.add("1\n2,3"));
 console.log(calculator.add("//|\n1,2,3"));//TDD test case which will provide output as 1 because delimiter is changed to | this but seperated by ,
 console.log(calculator.add("//;\n1;2"));  //Changed the delimiter to ;   
 console.log(calculator.add("//|\n1|2|3"));//Changed the delimiter to |
+    console.log(calculator.add("1,-2,3,-4"));   // Exception: negative numbers not allowed: -2, -4
+} catch (e) {
+    console.error(e.message);
+}
